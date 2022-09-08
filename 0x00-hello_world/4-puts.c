a6d44b0bc0da792ee54d9755d69926d80a491659
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-08 04:17:57 -0700

    Modified file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-char text[] = "\"Programming is like building a multilingual puzzle";
-
-puts(text);
-return (0);
+	char text[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(text);
+	return (0);
 }
