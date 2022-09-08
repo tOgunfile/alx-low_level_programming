10167fae528d08c390b9d7199abe059409d40625
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-08 05:01:31 -0700

    Modified file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int long_long_integer;
 	float floating_points;
 
-	printf("Size of a char: %c bytes(s)", sizeof(single_char));
-	printf("Size of a int: %d bytes(s)", sizeof(integer));
-	printf("Size of a long int: %d bytes(s)", sizeof(long_integer));
-	printf("Size of a long long int: %d bytes(s)", sizeof(long_long_integer));
-	printf("Size of a float: %f bytes(s)", sizeof(floating_points));
+	printf("Size of a char: %d bytes(s)\n", sizeof(single_char));
+	printf("Size of a int: %d bytes(s)\n", sizeof(integer));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long_integer));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long_long_integer));
+	printf("Size of a float: %d bytes(s)\n", sizeof(floating_points));
 
 	return (0);
 }
