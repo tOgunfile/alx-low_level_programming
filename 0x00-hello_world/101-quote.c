a27720693b8ac6693a1e2f99ab157adbabc6b353
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-08 06:28:45 -0700

    Modified file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Prints a string into standard error.
- *
+ * main - Prints string to standard output
  * Return: 1
  */
 int main(void)
 {
-	char err1[] = "and that piece of art is useful\"";
-	char err2[] = " - Dora Korpar, 2015-10-19\n";
-	
-	char err_message[] = strcat(err1, err2);
-	fwrite(err_message, 59, 1, stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
