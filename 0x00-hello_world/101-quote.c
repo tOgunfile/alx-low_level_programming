96f879ce42511aa89597c97c1205a1918ced47a1
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-08 05:58:47 -0700

    Modified file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char err1[] = "and that piece of art is useful\""
-	char err2[] = " - Dora Korpar, 2015-10-19\n"
+	char err1[] = "and that piece of art is useful\"";
+	char err2[] = " - Dora Korpar, 2015-10-19\n";
 	
-	char err_message[] = err1 + err2
+	char err_message[] = err1 + err2;
 	fwrite(err_message, 59, 1, stderr);
 	return (1);
 }
