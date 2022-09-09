9f5f16cf91e787e065c39251be0b43208610fcf2
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-09 09:05:19 -0700

    Created file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out a to z.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
