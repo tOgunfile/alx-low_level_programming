b0b78e68a05954020d18aa92511cd09286948ade
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-09 08:26:46 -0700

    Created file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out a to z.
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
