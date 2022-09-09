a905d244913e2d8172bcd1464bcf3f52f8f2ca2f
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-09 09:31:21 -0700

    Created file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out a to z.
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
