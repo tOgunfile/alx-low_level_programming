4f60eb30d77a8646f130a09c0eeac779028ad766
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-09 10:16:34 -0700

    Modified file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	for (n = '1'; n <= '9'; n++)
 	{
-		putchar(n);
-		putchar(',');
 		putchar(' ');
+		putchar(',');
+		putchar(n);
 	}
-	putchar('\n');
 	return (0);
 }
