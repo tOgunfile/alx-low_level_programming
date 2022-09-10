293cbca4fa04b21568c31cb0bb0cb0d152e4b6d8
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2022-09-09 21:49:30 -0700

    Created file 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints out a to z except q and e.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = '0'; n <= '99'; n++)
+	{
+		for (a = '0'; a <= '99'; a++)
+		{
+		if (n < a && n != j)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
